0eb76e78a4ce67d16f3f75180066c7b22a1ae32b
johackim <contact@johackim.com>
2016-09-09 23:00:37 +0200

    Update error messages

--- src/utils.js
+++ src/utils.js
@@ -2,8 +2,12 @@ export function handleErrorMessage(errorCode, error) {
     switch (errorCode) {
     case 5:
         throw new Error('Login error, VPN ?');
+    case 11:
+        throw new Error(`Two-factor authentication needed: ${error.error.verification_url}`);
     case 12:
         throw new Error('Invalid login');
+    case 13:
+        throw new Error('Invalid password');
     default:
         throw new Error(error);
     }
