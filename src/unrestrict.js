449c60d3806e18725a26034bd6b569c87d3dc75d
jcherqui <joachim.cherqui@viacesi.fr>
2017-03-23 23:14:57 +0100

    Re-add handleErrorMessage

--- src/unrestrict.js
+++ src/unrestrict.js
@@ -1,20 +1,24 @@
 import config from 'config';
 import debug from 'debug';
 import fetch from 'node-fetch';
+import handleErrorMessage from './utils';
 
 const log = debug('unrestrict');
 
 export default async (link, token) => {
     log(`unrestrict link ${link}`);
 
-    const url = `${config.apiEndpoint}/unrestrict/link?auth_token=${token}`;
-    const res = await fetch(url, {
-        method: 'POST',
-        body: JSON.stringify({
-            link,
-        }),
-        headers: { 'Content-Type': 'application/json' },
-    });
-
-    return (await res.json()).download;
+    try {
+        const url = `${config.apiEndpoint}/unrestrict/link?auth_token=${token}`;
+        const res = await fetch(url, {
+            method: 'POST',
+            body: JSON.stringify({
+                link,
+            }),
+            headers: { 'Content-Type': 'application/json' },
+        });
+        return (await res.json()).download;
+    } catch (e) {
+        return handleErrorMessage(e);
+    }
 };
