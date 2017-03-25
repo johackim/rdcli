63051f8ee1de8416ad1ffa6f307e48f7594bfd6c
jcherqui <joachim.cherqui@viacesi.fr>
2017-03-25 18:16:34 +0100

    Add rdcli test

--- test/specs/rdcli.spec.js
+++ test/specs/rdcli.spec.js
@@ -1,4 +1,10 @@
-// @TODO
-describe('rdcli', () => {
+import { exec } from 'child_process';
 
+describe('rdcli', () => {
+    it('should exec rdcli command', () => {
+        exec('node ./build/rdcli.js', (error, stdout, stderr) => {
+            assert.isNotNull(stdout);
+            assert.equal(stderr, '');
+        });
+    });
 });
