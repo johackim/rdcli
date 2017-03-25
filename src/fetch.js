f2d2cf6252499dc8a32d9b823d9eb0d99632b29f
ston3o <5039608+ston3o@users.noreply.github.com>
2017-03-25 20:35:48 +0100

    Fix parsing

--- src/fetch.js
+++ src/fetch.js
@@ -2,6 +2,15 @@ import fetch from 'node-fetch';
 import querystring from 'querystring';
 import handleErrorMessage from './utils';
 
+const IsJsonString = (str) => {
+    try {
+        JSON.parse(str);
+    } catch (e) {
+        return false;
+    }
+    return true;
+};
+
 export default async function (url, opts = { method: 'GET' }) {
     const res = await fetch(url, {
         method: opts.method,
@@ -9,15 +18,16 @@ export default async function (url, opts = { method: 'GET' }) {
         body: (typeof opts.body !== 'undefined' && opts.body.constructor.name === 'Buffer') ? opts.body : querystring.stringify(opts.body),
     });
 
-    let data = await res.text();
+    const data = await res.text();
 
     if (!data) return null;
 
-    data = JSON.parse(data);
-
     if (![200, 201].includes(res.status)) {
-        return handleErrorMessage(data);
+        if (!IsJsonString(data)) {
+            throw new Error('Internal error');
+        }
+        return handleErrorMessage(JSON.parse(data));
     }
 
-    return data;
+    return JSON.parse(data);
 }
