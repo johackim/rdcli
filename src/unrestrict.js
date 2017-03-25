af9906c54df759449f904b62657117ab219cb935
jcherqui <joachim.cherqui@viacesi.fr>
2017-03-25 16:25:04 +0100

    Fix await/async

--- src/unrestrict.js
+++ src/unrestrict.js
@@ -1,24 +1,16 @@
 import config from 'config';
 import debug from 'debug';
-import fetch from 'node-fetch';
-import handleErrorMessage from './utils';
+import fetch from './fetch';
 
 const log = debug('unrestrict');
 
 export default async (link, token) => {
     log(`unrestrict link ${link}`);
 
-    try {
-        const url = `${config.apiEndpoint}/unrestrict/link?auth_token=${token}`;
-        const res = await fetch(url, {
-            method: 'POST',
-            body: JSON.stringify({
-                link,
-            }),
-            headers: { 'Content-Type': 'application/json' },
-        });
-        return (await res.json()).download;
-    } catch (e) {
-        return handleErrorMessage(e);
-    }
+    const data = await fetch(`${config.apiEndpoint}/unrestrict/link?auth_token=${token}`, {
+        method: 'POST',
+        body: { link },
+    });
+
+    return data.download;
 };
