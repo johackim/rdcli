525fdf24c777989a267951c143ad58a52fb26e2b
stoneo <stoneo@users.noreply.github.com>
2017-04-22 23:25:43 +0200

    Allow remote traffic

--- test/specs/unrestrict.spec.js
+++ test/specs/unrestrict.spec.js
@@ -19,4 +19,24 @@ describe('unrestrict', () => {
         const unrestrictLink = await unrestrict(link, token);
         assert.equal(unrestrictLink, 'https://100.download.real-debrid.com/d/4ALWGL4BN4C4G/test.rar');
     });
+
+    it('should unrestrict link with remote traffic', async () => {
+        const token = 'APS7T57AXM7G3U7KCT57NYCVAY';
+        server.post('/unrestrict/link', (req, res) => res.json({
+            id: '4ALWGL4BN4C4G',
+            filename: 'test.rar',
+            filesize: 200000000,
+            link: 'http://uptobox.com/xxxxxxxxxxxx',
+            host: 'uptobox.com',
+            chunks: 16,
+            crc: 1,
+            download: 'https://100.download.real-debrid.com/d/4ALWGL4BN4C4G/test.rar',
+            streamable: 0,
+        }));
+
+        const link = 'http://uptobox.com/xxxxxxxxxxxx';
+        const remote = true;
+        const unrestrictLink = await unrestrict(link, token, remote);
+        assert.equal(unrestrictLink, 'https://100.download.real-debrid.com/d/4ALWGL4BN4C4G/test.rar');
+    });
 });
